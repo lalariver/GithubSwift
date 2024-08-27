fda82b70829516b3cd3e447f6700a1f18630fd5d
Luke <loveriverain@gmail.com>
2024-08-27 18:18:29 +0800

    [add] throws enum 改為 struct 控管

--- GithubSwift/NetworkManager/GithubApi.swift
+++ GithubSwift/NetworkManager/GithubApi.swift
@@ -7,64 +7,95 @@
 
 import Foundation
 
-enum GithubApi {
-    case getUsers(since: String?)
-    case getUserDetail(login: String)
+// 定義一個協議，用來統一處理不同的 API 請求
+protocol APIRequest {
+    associatedtype ResponseDataType: Codable
     
+    var baseURL: String { get }
+    var path: String { get }
+    var method: String { get }
+    var headers: [String: String] { get }
+    var parameters: [String: Any]? { get }
+    
+    func makeRequest() throws -> URLRequest
+}
+
+// 統一處理 URLRequest 的生成
+extension APIRequest {
     var baseURL: String {
         return "https://api.github.com"
     }
     
-    var path: String {
-        switch self {
-        case .getUsers(let since):
-            var path = "/users?per_page=20"
-            if let since = since {
-                path += "&since=\(since)"
-            }
-            return path
-        case .getUserDetail(let login):
-            return "/users/\(login)"
-        }
-    }
-    
-    var method: String {
-        switch self {
-        case .getUsers:
-            return "GET"
-        case .getUserDetail:
-            return "GET"
-        }
-    }
-    
     var headers: [String: String] {
-        return [
-            "Accept": "application/vnd.github+json",
-            "Authorization": "Bearer <YOUR_TOKEN>",
-            "X-GitHub-Api-Version": "2022-11-28"
-        ]
-    }
-    
-    var parameters: [String: String]? {
-        switch self {
-        case .getUsers:
-            return nil
-        case .getUserDetail:
-            return nil
+            return [
+                "Accept": "application/vnd.github+json",
+                "X-GitHub-Api-Version": "2022-11-28"
+            ]
         }
-    }
     
-    var urlRequest: URLRequest {
-        let urlString = baseURL + path
-        let url = URL(string: urlString)!
-        var request = URLRequest(url: url)
+    func makeRequest() throws -> URLRequest {
+        guard let url = URL(string: baseURL + path) else {
+            throw APIError.urlError
+        }
+        
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
+        if let parameters = parameters {
+            urlComponents.queryItems = parameters.map { URLQueryItem(name: $0.key, value: "\($0.value)") }
+        }
+        
+        var request = URLRequest(url: urlComponents.url!)
         request.httpMethod = method
         request.allHTTPHeaderFields = headers
         
-        if let parameters = parameters {
-            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        return request
+    }
+}
+
+enum GithubApi {
+    // 定義一個具體的 API 請求
+    struct GetUsersRequest: APIRequest {
+        typealias ResponseDataType = [GithubUser]
+        
+        var path: String {
+            if let since = since, !since.isEmpty {
+                return "/users?per_page=20&since=\(since)"
+            } else {
+                return "/users?per_page=20"
+            }
         }
         
-        return request
+        var method: String {
+            return "GET"
+        }
+        
+        var parameters: [String: Any]?
+        
+        private var since: String?
+        
+        init(since: String?) {
+            self.since = since
+        }
+    }
+
+    // 定義另一個具體的 API 請求
+    struct GetUserDetailRequest: APIRequest {
+        typealias ResponseDataType = GithubUserDetailModel
+        
+        var path: String {
+            return "/users/\(login)"
+        }
+        
+        var method: String {
+            return "GET"
+        }
+        
+        var parameters: [String: Any]? = nil
+        
+        private var login: String
+        
+        init(login: String) {
+            self.login = login
+        }
     }
 }
